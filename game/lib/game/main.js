0a1baba9c4bb15d866f92a91c0ec39cf48b8ae64
Philip <philip@bureaubureau.de>
2012-08-10 16:37:52 +0200

    Works in Mobile Safari

--- game/lib/game/main.js
+++ game/lib/game/main.js
@@ -45,7 +45,7 @@ MyGame = ig.Game.extend({
         if ( !this.touchColors[t] ) this.touchColors[t] = this.randomColorStr()
         
         ig.system.context.fillStyle = this.touchColors[t]
-        ig.system.context.fillRect( ig.input.touches[t].x - 20, ig.input.touches[t].y - 20, 40, 40 )
+        ig.system.context.fillRect( ig.input.touches[t].x - 40, ig.input.touches[t].y - 40, 80, 80 )
       }
     }
   },
